67cecb6fc81753b9fa8fb29f88cb79afd9618fd9
John D. Wojton <Johnwojton@hotmail.com>
2017-09-04 20:39:58 -0400

    Finished the initial Waist to Height Ratio

--- HealthSuite/src/test_ui/W_to_H_Ratio.java
+++ HealthSuite/src/test_ui/W_to_H_Ratio.java
@@ -7,6 +7,8 @@ package test_ui;
 
 import java.awt.Color;
 import java.awt.GridBagConstraints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -15,7 +17,7 @@ import javax.swing.JTextField;
  *
  * @author JohnW
  */
-public class W_to_H_Ratio extends PanelParent
+public class W_to_H_Ratio extends PanelParent implements ActionListener
 {
     GridBagConstraints gbc = new GridBagConstraints();
     JLabel     Waist;
@@ -24,6 +26,11 @@ public class W_to_H_Ratio extends PanelParent
     JTextField EnterHeight;
     JButton    Calculate;
     XMLFileController XML;
+    double WaistInt;
+    double HeightInt;
+    JLabel Total;
+    String Total_str;
+    
 
     W_to_H_Ratio()
     {
@@ -34,6 +41,10 @@ public class W_to_H_Ratio extends PanelParent
     
     void InitVar()
     {
+        Total_str = "Your Waist to  Height Ratio is: ";
+        WaistInt = 0;
+        HeightInt = 0;
+        Total = new JLabel("");
         this.setBackground(Color.red);
         Waist  = new JLabel("Waist in Inches");
         Height = new JLabel("Height in Inches");
@@ -56,5 +67,31 @@ public class W_to_H_Ratio extends PanelParent
          gbc.gridx = 1;
          gbc.gridy = 1;
          this.add(EnterHeight, gbc);
+         
+         gbc.gridx = 0;
+         gbc.gridy = 2;
+         Calculate.addActionListener(this);
+         this.add(Calculate);
+               
+         gbc.gridx = 0;
+         gbc.gridy = 5;
+         this.add(Total);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+      Object o = e.getSource();
+      if(o == Calculate)
+      {
+          System.out.println("W_To_H Clicked");
+           double total = 0;
+           WaistInt = (double) Integer.parseInt(EnterWaist.getText());
+           HeightInt = (double) Integer.parseInt(EnterHeight.getText());
+           System.out.println("Stuff " + WaistInt + HeightInt);
+           total = (double) ((WaistInt / HeightInt));
+           System.out.println("Total: " + total);
+           Total.setText(Total_str + total);
+      }
     }
 }
